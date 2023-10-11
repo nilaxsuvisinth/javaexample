1e4487a74b735222ceb3421cabdc28315ec3c811
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2023-10-11 13:41:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log("Welcome October");
+document.write("Welcome to Leo");
+// alert("Please alert, Lion is on the way");
+// window.alert("Mr.Uthai, this is for you");
+document.getElementById("demo").innerHTML = 2*7
+
+var first_name=("Nilax");
+var second_name=("Suvisinth");
+console.log(first_name+" "+second_name);
+console.log(first_name.concat(second_name));
+let x=2;
+let y=7;
+console.log(x+y)
+
+let n=7;
+ n=3;
+console.log(n);
+var q=17;
+    q=16;
+console.log(q)
+
+// const w=33;
+//     w=35;
+// console.log(w)
+
+var s=4;
+var d=6;
+console.log(s+d);
+var s=8;
+console.log(s+d);
+
+var p=25;
+{
+    var p=50;
+    console.log(p)
+}
+console.log(p)
+
+let j=33;
+{
+    let j=44;
+    console.log(j)
+}
+console.log(j)
+
+const k=99;
+{
+    const k=1;
+    console.log(k)
+}
+console.log(k)
+
+
+var f
+console.log(f)
+
+
+// let my_name=("vijay")
+// console.log(my_name)
+
---
+++ script2.js
@@ -0,0 +1,52 @@
+let messy_number=(1234.56789);
+let clean_number=messy_number.toPrecision(4);
+console.log(clean_number)
+
+// var x=2.99
+// var x
+// x=Math.round(x);
+// // x=Math.floor(x);
+// // x=Math.ceil(x)
+// console.log(x)
+
+console.log(Math.round(20.77))
+console.log(Math.round(20.49))
+console.log(Math.round(20.50))
+
+console.log(Math.ceil(15.77))
+console.log(Math.ceil(15.49))
+console.log(Math.ceil(15.50))
+
+console.log(Math.floor(17.77))
+console.log(Math.floor(17.49))
+console.log(Math.floor(17.50))
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+
+firstValue=1;
+numberOfChoices=6;
+randomNumber=Math.floor(Math.random()*numberOfChoices+firstValue);
+console.log('Random Number :', randomNumber);
+
+let a = 3;
+let b = new Number(3);
+let c = 3;
+
+console.log(a == b);
+console.log(a === b);
+console.log(b === c);
+
+let number = 0;
+console.log(number++);
+console.log(++number);
+console.log(number);
+
+const sum = eval('10*10+5');
+console.log(sum);
+
+var num = 8;
+var num = 10;
+
+console.log(num);
